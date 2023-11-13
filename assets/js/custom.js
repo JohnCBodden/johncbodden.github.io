5795e83516d0d6cf043006d7ef0cea2da8f47f7b
JohnCBodden <john.bodden@hotmail.com>
2023-11-13 18:01:06 -0500

    Add files via upload
    updated index and course pages

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -12,3 +12,22 @@ for (i = 0; i < acc.length; i++) {
     } 
   });
 }
+
+/*Scroll to top when arrow up clicked BEGIN*/
+$(window).scroll(function() {
+    var height = $(window).scrollTop();
+    if (height > 100) {
+        $('#back2Top').fadeIn();
+    } else {
+        $('#back2Top').fadeOut();
+    }
+});
+$(document).ready(function() {
+    $("#back2Top").click(function(event) {
+        event.preventDefault();
+        $("html, body").animate({ scrollTop: 0 }, "slow");
+        return false;
+    });
+
+});
+ /*Scroll to top when arrow up clicked END*/
